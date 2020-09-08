cdfb2422f2705b30e680e141d27846a8d8b6d9fb
sanja avi <avi.sanja@gmail.com>
2020-09-08 08:48:38 +0700

    feature/Data collection
    -fix() disable id for users array

--- app/models/Organization.js
+++ app/models/Organization.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose  = require('mongoose');
 const OrganizationSchema = new mongoose.Schema({
     name :{
         type:String,
         
     },
     users :[{
-      _id:false,  
-      userId:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
-      role:{type:String,enum:['Owner','Manager','Member'], default:'Member'}
-      }]
+        _id:false,
+        userId:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+        role:{type:String,enum:['Owner','Manager','Member'], default:'Member'}
+    }],
+    project :[{
+        type:mongoose.Schema.Types.ObjectId,ref:'Project'
+    }]
 },{
     timestamps: true
-});
-
+})
 
 module.exports = mongoose.model('Organization',OrganizationSchema); 
--- app/models/Project.js
+++ app/models/Project.js
@@ -11,16 +11,14 @@ const ProjSchema = new mongoose.Schema({
         userId:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
         role:{type:String,enum:['Owner','Manager','Member'], default:'Member'}
     }],
+    organizationId :[{
+        type:mongoose.Schema.Types.ObjectId,ref:'Organization'
+    }],
     task:[{
         type:mongoose.Schema.Types.ObjectId,ref:'Task'
     }]
    
-    /*Created:{
-        type:Date, default:Date.now
-    },
-    Updated:{
-        type:Date, default:Date.now
-    }*/
+   
 },{
     timestamps:true
 })
